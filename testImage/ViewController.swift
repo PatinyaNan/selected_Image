55e3aff2a81b50fa6ff22156569ea550e9be4b30
Admin <admin@Admins-iMac-2.local>
2019-01-30 16:12:05 +0700

    first commit

--- testImage/ViewController.swift
+++ testImage/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func chooseImage(_ sender: Any) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+//            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
+//                imagePickerController.sourceType = .camera
+//                self.present(imagePickerController, animated: true, completion: nil)
+//            }))
+            actionSheet.addAction(UIAlertAction(title: "Photo Libary", style: .default, handler: { (action:UIAlertAction) in
+                imagePickerController.sourceType = .photoLibrary
+                self.present(imagePickerController, animated: true, completion: nil)
+            }))
+            actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:  nil))
+            self.present(actionSheet, animated: true, completion: nil)
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            let image = info[.originalImage] as! UIImage
+            imageView.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
